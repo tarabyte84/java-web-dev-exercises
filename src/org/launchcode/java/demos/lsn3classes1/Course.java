5e844765d37d00c534a1a26e1ed6e5c3861870c8
tarabyte84 <tekotthoff@me.com>
2020-12-08 23:06:12 -0600

    Changes to Course.

--- src/org/launchcode/java/demos/lsn3classes1/Course.java
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -1,5 +1,7 @@
 package org.launchcode.java.demos.lsn3classes1;
 
+import java.util.ArrayList;
+
 public class Course {
 
 //    -String teacher
@@ -8,11 +10,23 @@ public class Course {
 //
 //    rosterData()
 //        - an array list of student objects
+//          - each student is a hashmap
 
     private String teacher;
     private int credits;
-    private ArrayList<> roster;
+    private ArrayList<String> roster;
 
-    public Course() {
+    public Course(String teacher, int credits, ArrayList<String> roster) {
+        this.teacher = teacher;
+        this.credits = credits;
+        this.roster = roster;
     }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+//    public void setTeacher(String newTeacher) {
+//
+//    }
 }
